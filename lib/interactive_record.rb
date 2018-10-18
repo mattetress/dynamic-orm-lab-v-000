b5531e95d2ac4d1230ec437308fcd3c176f5f7c3
Matt Etress <mattetress@me.com>
2018-10-18 05:28:42 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,10 +1,11 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
+require 'pry'
 
 class InteractiveRecord
 
   def self.table_name
     self
-    binding.pry 
+    binding.pry
   end
 end
